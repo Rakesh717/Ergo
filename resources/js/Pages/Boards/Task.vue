5f07e6a18b28be3a1a7dcc24e60d0461d6d2ad04
Rakesh Thapa <rakeshthapa358@gmail.com>
2023-01-11 00:06:16 +0545

    wip

--- resources/js/Pages/Boards/Task.vue
+++ resources/js/Pages/Boards/Task.vue
@@ -27,7 +27,9 @@ function updateDueDate(date) {
 }
 
 function formatDueDate(date) {
-    return format(parseISO(date), "MMM do, yyyy h:mm a");
+    date = parseISO(date);
+
+    return format(date, "MMM do, h:mm a");
 }
 
 function toggleComplete() {
@@ -41,69 +43,7 @@ function toggleComplete() {
 
 <template>
     <Link href="#" class="block bg-white shadow px-4 py-4 rounded-md">
-        <p>{{ task.title }}</p>
-
-        <div class="mt-5 flex justify-between items-center">
-            <div class="space-x-3 flex items-center">
-                <button
-                    :class="[
-                        task.assignee
-                            ? ''
-                            : 'border-2 border-dashed border-gray-300 rounded-full p-1 text-gray-500 hover:text-gray-700 hover:border-gray-700 transition',
-                    ]"
-                    @click.prevent="selectMember = true"
-                >
-                    <svg
-                        v-if="!task.assignee"
-                        class="w-4 h-4"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                        ></path>
-                    </svg>
-                    <img
-                        class="h-8 w-8 object-cover rounded-full"
-                        :src="task.assignee.profile_photo_url"
-                        :alt="task.assignee.name"
-                        v-else
-                    />
-                </button>
-                <button
-                    :class="
-                        task.due_at
-                            ? ''
-                            : 'border-2 border-dashed border-gray-300 rounded-full p-1 text-gray-500 hover:text-gray-700 hover:border-gray-700 transition'
-                    "
-                    @click.prevent="showDateTimePicker = true"
-                >
-                    <svg
-                        v-if="!task.due_at"
-                        class="w-4 h-4"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                        ></path>
-                    </svg>
-                    <p class="hover:bg-gray-50 rounded-md py-2 px-3" v-else>
-                        {{ formatDueDate(task.due_at) }}
-                    </p>
-                </button>
-            </div>
-
+        <div class="flex items-center space-x-2">
             <button
                 :class="[
                     task.is_completed
@@ -114,7 +54,51 @@ function toggleComplete() {
                 @click.prevent="toggleComplete"
             >
                 <svg
-                    class="w-5 h-5"
+                    class="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    v-if="!task.is_completed"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                </svg>
+                <svg
+                    class="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    v-else
+                >
+                    <path
+                        clip-rule="evenodd"
+                        fill-rule="evenodd"
+                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    ></path>
+                </svg>
+            </button>
+
+            <p>{{ task.title }}</p>
+        </div>
+
+        <div class="mt-5 flex justify-between items-center">
+            <button
+                :class="
+                    task.due_at
+                        ? ''
+                        : 'border-2 border-dashed border-gray-300 rounded-full p-1 text-gray-500 hover:text-gray-700 hover:border-gray-700 transition'
+                "
+                @click.prevent="showDateTimePicker = true"
+            >
+                <svg
+                    v-if="!task.due_at"
+                    class="w-4 h-4"
                     fill="none"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
@@ -124,9 +108,43 @@ function toggleComplete() {
                         stroke-linecap="round"
                         stroke-linejoin="round"
                         stroke-width="2"
-                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                     ></path>
                 </svg>
+                <p class="hover:bg-gray-50 rounded-md py-2 px-3" v-else>
+                    {{ formatDueDate(task.due_at) }}
+                </p>
+            </button>
+
+            <button
+                :class="[
+                    task.assignee
+                        ? ''
+                        : 'border-2 border-dashed border-gray-300 rounded-full p-1 text-gray-500 hover:text-gray-700 hover:border-gray-700 transition',
+                ]"
+                @click.prevent="selectMember = true"
+            >
+                <svg
+                    v-if="!task.assignee"
+                    class="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                    ></path>
+                </svg>
+                <img
+                    class="h-8 w-8 object-cover rounded-full"
+                    :src="task.assignee.profile_photo_url"
+                    :alt="task.assignee.name"
+                    v-else
+                />
             </button>
         </div>
     </Link>
